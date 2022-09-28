8d942b8f4f0fb43ee25b94485d2a498e8c8e53a1
VadimKruchkov <Vtradecapital@gmail.com>
2022-09-28 18:16:41 +0300

    add all tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const nameColorEl = document.querySelector(".color");
+
+function changeBgColor() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  nameColorEl.textContent = randomColor;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonEl.addEventListener("click", changeBgColor);
